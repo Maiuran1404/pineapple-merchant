3c992a1e3cb2c2bd441bbdf408fbc610aacb269e
Maiuran <maiuran1404@gmail.com>
2024-02-02 15:01:59 +0100

    new typos

--- src/components/OrdersPage.tsx
+++ src/components/OrdersPage.tsx
@@ -35,7 +35,7 @@ function OrdersPage() {
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
             <h1 className="text-base font-semibold leading-6 text-gray-900">
-              Orders
+              Orders final
             </h1>
             <p className="mt-2 text-sm text-gray-700">
               Orders from customers will appear here.
