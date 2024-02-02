a543ebb4442d85edc89af79a59b520e2a7ce8ba1
Maiuran1404 <maiuran.loganathan@uni.minerva.edu>
2024-02-02 21:34:09 +0100

    finally yellooww

--- src/constants/sampleProducts.ts
+++ src/constants/sampleProducts.ts
@@ -8,8 +8,6 @@ export const sampleProducts: ItemProps[] = [
     description: "Keep your beverages hot or cold all day.",
     price: 35.0,
     inStock: true,
-    image:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-03.jpg",
     imageAlt: "Insulated bottle with white base and black snap lid.",
   },
   {
@@ -18,8 +16,6 @@ export const sampleProducts: ItemProps[] = [
     description: "Look fresh in this cotton tee.",
     price: 32,
     inStock: true,
-    image:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg",
     imageAlt: "Front of men's Basic Tee in sienna.",
   },
   // More products...
