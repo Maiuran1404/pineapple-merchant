a907086e387f549992e2acc575f88cd4ee74360d
Maiuran1404 <maiuran.loganathan@uni.minerva.edu>
2024-02-02 22:18:19 +0100

    finally yellooww

--- src/providers/ProductsProvider.tsx
+++ src/providers/ProductsProvider.tsx
@@ -1,125 +1,125 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import React, { createContext, useCallback, useState } from "react";
-import {
-  addStoreItem,
-  getStoreItems,
-  removeStoreItem,
-  updateStoreItem,
-} from "~/apiEndpoints";
-import type { ItemProps } from "~/constants/orders";
-import { sampleProducts } from "~/constants/sampleProducts";
-import useStore from "~/hooks/useStore";
+// import { useQuery, useQueryClient } from "@tanstack/react-query";
+// import React, { createContext, useCallback, useState } from "react";
+// import {
+//   addStoreItem,
+//   getStoreItems,
+//   removeStoreItem,
+//   updateStoreItem,
+// } from "~/apiEndpoints";
+// import type { ItemProps } from "~/constants/orders";
+// import { sampleProducts } from "~/constants/sampleProducts";
+// import useStore from "~/hooks/useStore";
 
-interface ProductsContextType {
-  isLoading: boolean;
-  error: unknown;
-  products: ItemProps[];
-  addProduct: (product: ItemProps) => void;
-  removeProduct: (product: ItemProps) => void;
-  updateProduct: (updatedProduct: ItemProps) => void;
-}
+// interface ProductsContextType {
+//   isLoading: boolean;
+//   error: unknown;
+//   products: ItemProps[];
+//   addProduct: (product: ItemProps) => void;
+//   removeProduct: (product: ItemProps) => void;
+//   updateProduct: (updatedProduct: ItemProps) => void;
+// }
 
-// Define the context
-export const ProductsContext = createContext<ProductsContextType | undefined>(
-  undefined,
-);
+// // Define the context
+// export const ProductsContext = createContext<ProductsContextType | undefined>(
+//   undefined,
+// );
 
-function ProductsProvider({ children }: { children: React.ReactNode }) {
-  // Access the client
-  const { store } = useStore();
-  const queryClient = useQueryClient();
-  const [productss, setProducts] = useState<ItemProps[]>(sampleProducts);
+// function ProductsProvider({ children }: { children: React.ReactNode }) {
+//   // Access the client
+//   const { store } = useStore();
+//   const queryClient = useQueryClient();
+//   const [productss, setProducts] = useState<ItemProps[]>(sampleProducts);
 
-  const {
-    isLoading,
-    error,
-    data: products,
-  } = useQuery({
-    queryKey: ["products", store?.id],
-    queryFn: () => getStoreItems(store.id),
-    enabled: !!store?.id,
-  });
+//   const {
+//     isLoading,
+//     error,
+//     data: products,
+//   } = useQuery({
+//     queryKey: ["products", store?.id],
+//     queryFn: () => getStoreItems(store.id),
+//     enabled: !!store?.id,
+//   });
 
-  const addProduct = useCallback(
-    async (product: ItemProps) => {
-      try {
-        const resp = await addStoreItem(store?.id, product);
+//   const addProduct = useCallback(
+//     async (product: ItemProps) => {
+//       try {
+//         const resp = await addStoreItem(store?.id, product);
 
-        if (resp) {
-          await queryClient.invalidateQueries({
-            queryKey: ["products", store.id],
-          });
-        }
+//         if (resp) {
+//           await queryClient.invalidateQueries({
+//             queryKey: ["products", store.id],
+//           });
+//         }
 
-        return resp;
-      } catch (err) {
-        console.error(err);
-        return null;
-      } finally {
-        await queryClient.invalidateQueries({
-          queryKey: ["products", store.id],
-        });
-      }
-    },
-    [store, queryClient],
-  );
+//         return resp;
+//       } catch (err) {
+//         console.error(err);
+//         return null;
+//       } finally {
+//         await queryClient.invalidateQueries({
+//           queryKey: ["products", store.id],
+//         });
+//       }
+//     },
+//     [store, queryClient],
+//   );
 
-  const removeProduct = useCallback(
-    async (product: ItemProps) => {
-      try {
-        await removeStoreItem(store?.id, product.id).then(() => {
-          void queryClient.invalidateQueries({
-            queryKey: ["products", store.id],
-          });
-        });
-      } catch (err) {
-        console.error(err);
-      } finally {
-        await queryClient.invalidateQueries({
-          queryKey: ["products", store.id],
-        });
-      }
-    },
-    [store, queryClient],
-  );
+//   const removeProduct = useCallback(
+//     async (product: ItemProps) => {
+//       try {
+//         await removeStoreItem(store?.id, product.id).then(() => {
+//           void queryClient.invalidateQueries({
+//             queryKey: ["products", store.id],
+//           });
+//         });
+//       } catch (err) {
+//         console.error(err);
+//       } finally {
+//         await queryClient.invalidateQueries({
+//           queryKey: ["products", store.id],
+//         });
+//       }
+//     },
+//     [store, queryClient],
+//   );
 
-  const updateProduct = useCallback(
-    async (updatedProduct: ItemProps) => {
-      try {
-        const resp = await updateStoreItem(store?.id, updatedProduct);
-        if (resp) {
-          await queryClient.invalidateQueries({
-            queryKey: ["products", store.id],
-          });
-        }
+//   const updateProduct = useCallback(
+//     async (updatedProduct: ItemProps) => {
+//       try {
+//         const resp = await updateStoreItem(store?.id, updatedProduct);
+//         if (resp) {
+//           await queryClient.invalidateQueries({
+//             queryKey: ["products", store.id],
+//           });
+//         }
 
-        return resp;
-      } catch (err) {
-        console.error(err);
-        return null;
-      } finally {
-        await queryClient.invalidateQueries({
-          queryKey: ["products", store.id],
-        });
-      }
-    },
-    [store, queryClient],
-  );
+//         return resp;
+//       } catch (err) {
+//         console.error(err);
+//         return null;
+//       } finally {
+//         await queryClient.invalidateQueries({
+//           queryKey: ["products", store.id],
+//         });
+//       }
+//     },
+//     [store, queryClient],
+//   );
 
-  const value = {
-    isLoading,
-    error,
-    products,
-    addProduct,
-    removeProduct,
-    updateProduct,
-  };
+//   const value = {
+//     isLoading,
+//     error,
+//     products,
+//     addProduct,
+//     removeProduct,
+//     updateProduct,
+//   };
 
-  return (
-    <ProductsContext.Provider value={value}>
-      {children}
-    </ProductsContext.Provider>
-  );
-}
+//   return (
+//     <ProductsContext.Provider value={value}>
+//       {children}
+//     </ProductsContext.Provider>
+//   );
+// }
 
-export default ProductsProvider;
+// export default ProductsProvider;
