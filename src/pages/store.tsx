3af73c78dcb0b6aaa079fbd117e191ff9d9a214e
Maiuran1404 <maiuran.loganathan@uni.minerva.edu>
2024-02-05 16:16:44 +0100

    fixes

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -104,7 +104,7 @@ const Store = () => {
 
   return (
     <h1> 
-      Store testing
+      Store testingg
     </h1>
     // <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
     //   {/* <input type="text" name="name" value={shop.name} onChange={handleInputChange} placeholder="Shop Name" />
