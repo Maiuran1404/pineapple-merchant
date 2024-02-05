29fcff873d2c7b0d99abf605c8e688cff6091832
Maiuran1404 <maiuran.loganathan@uni.minerva.edu>
2024-02-05 16:07:07 +0100

    fixes

--- firebase.ts
+++ firebase.ts
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+import 'dotenv/config';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; // Import Firebase Authentication
 import { getFirestore } from "firebase/firestore";
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDrjWvEIiIr190GIvQxCSJmksRiYcKaLqY",
-  authDomain: "pineapple-28.firebaseapp.com",
-  projectId: "pineapple-28",
-  storageBucket: "pineapple-28.appspot.com",
-  messagingSenderId: "919022920074",
-  appId: "1:919022920074:web:1dec77f80ee7a3e04c2873",
-  measurementId: "G-CQJP0C1489",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
