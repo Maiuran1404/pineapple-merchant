bc89c0a6161d0ee931ba0c298518d650ce63fefd
Maiuran1404 <maiuran.loganathan@uni.minerva.edu>
2024-02-13 10:36:56 +0100

    form updates

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -2,22 +2,29 @@ import { useState, ChangeEvent, FormEvent, useEffect } from "react";
 import { addFormDataToFirestore } from "~/apiEndpoints"; // Adjust the path as necessary
 import ShopForm from "~/components/ShopForm";
 import { Shop } from "~/types"; // Ensure this is correctly defined in your types
-import { useUser } from '@clerk/clerk-react';
+import { useUser } from "@clerk/clerk-react";
+import Container from "~/components/Container";
 
 const Store = () => {
   const { user } = useUser();
   const [shopId, setShopId] = useState<string | undefined>();
 
   useEffect(() => {
-    const shopIdFromMetadata = user?.publicMetadata?.shopId as string | undefined;
+    const shopIdFromMetadata = user?.publicMetadata?.shopId as
+      | string
+      | undefined;
     setShopId(shopIdFromMetadata);
   }, [user]);
 
   return (
-    <>
-      <h1>Store Setup</h1>
-      <ShopForm shopId={shopId}/>
-    </>
+    <Container>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+          Store Setup{" "}
+        </h1>
+      </div>
+      <ShopForm shopId={shopId} />
+    </Container>
   );
 };
 
