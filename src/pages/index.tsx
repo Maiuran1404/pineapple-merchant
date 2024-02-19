c4668a260230909a8b19d72a66bddaa55dc7ce92
Maiuran1404 <maiuran.loganathan@uni.minerva.edu>
2024-02-19 20:04:30 +0100

    Monday push trying to push the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
-import OrdersPage from "~/components/OrdersPage";
-
+// index.tsx
 export default function Home() {
-  return <OrdersPage />;
+  return (
+    <div>
+      <h1>Welcome to Pineapple</h1>
+      <p>Your social commerce platform for a connected and engaging shopping experience.</p>
+    </div>
+  );
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      showHeader: false, // Tell _app.tsx not to show the header on this page
+    },
+  };
 }
